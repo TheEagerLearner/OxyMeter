33afe05c8295b96595a31170c6ca7a95fbf6a358
TheEagerLearner <saurabhmul10@gmail.com>
2022-09-11 00:35:58 +0530

    lol changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -54,7 +54,7 @@ const Home = ({navigation}) => {
             />
             <Text
                 style={stylesheet.textheading}
-            >Oxygen purity</Text>
+            >Oxygen puurity</Text>
 
             <TouchableOpacity
                 style={stylesheet.about_btn}
